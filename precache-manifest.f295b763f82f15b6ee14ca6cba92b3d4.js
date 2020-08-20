47e4c2c892adcac03b3b25662d17dcc4d38d96bc
savezsiddiqui <savezsiddiqui@gmail.com>
2020-08-20 14:03:22 +0530

    Updates

---
+++ precache-manifest.f295b763f82f15b6ee14ca6cba92b3d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cb4aeec395ed8d90638d0689169d082",
+    "url": "/vii-sem/index.html"
+  },
+  {
+    "revision": "b79fb0d2d59b148e46df",
+    "url": "/vii-sem/static/css/main.28f1638b.chunk.css"
+  },
+  {
+    "revision": "44ed711b7da41ca75258",
+    "url": "/vii-sem/static/js/2.4a203992.chunk.js"
+  },
+  {
+    "revision": "241e2720c0228744c712295a835ee806",
+    "url": "/vii-sem/static/js/2.4a203992.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b79fb0d2d59b148e46df",
+    "url": "/vii-sem/static/js/main.258704f8.chunk.js"
+  },
+  {
+    "revision": "0aadaf95ad102b28cf65",
+    "url": "/vii-sem/static/js/runtime-main.33a2358b.js"
+  }
+]);
